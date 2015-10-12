41bbf8e0fd3a9390b2e620b843f42cd478ea12fb
FCO <fernandocorrea@gmail.com>
2015-10-12 14:48:16 -0300

    Initial Commit

---
+++ EventEmitter/EventEmitter.h
@@ -0,0 +1,19 @@
+//
+//  EventEmitter.h
+//  EventEmitter
+//
+//  Created by Fernando Oliveira on 12/10/15.
+//  Copyright © 2015 FCO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EventEmitter.
+FOUNDATION_EXPORT double EventEmitterVersionNumber;
+
+//! Project version string for EventEmitter.
+FOUNDATION_EXPORT const unsigned char EventEmitterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventEmitter/PublicHeader.h>
+
+
